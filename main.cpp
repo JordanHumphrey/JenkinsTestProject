0edb66b6bf39c9f9998dca5679253c485dc0dc42
Jordan Humphrey <jphumphrey2@gmail.com>
2020-11-18 17:09:00 -0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+using std::string;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    string str = "Hello world!";
+    cout << str << endl;
+    return 0;
+}
